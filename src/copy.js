a31d9ac272b11264eb62bc75f3f1da7d52373b1c
Jason Kao <jasonkao85@gmail.com>
2018-12-05 21:16:20 -0500

    copy

---
+++ src/copy.js
@@ -0,0 +1,28 @@
+const COPY = `[copy]
+division: Humanities
+[.steps]
+
+before: Let's first examine the faculty in the Humanities who <b>are not</b> eligible for tenure. From 2007 to 2017, the percentage of female faculty...
+
+after: <b>...has remained around 62%.</b> This majority has persisted over the decade despite the group having grown almost 40% in size.
+
+
+before: What about Humanities faculty who <b>are tenured</b>? In the graph below, estimate the female percentage of that group over the same time period (2007 to 2017). We have provided a starting point.
+
+after: In the past decade, though women have made up a strong majority of the Humanities faculty who are not eligible for tenure, female representation in tenured faculty positions has grown at a leaden rate.
+
+[]
+
+division: Natural Sciences
+[.steps]
+
+before: We see a similar story in the Natural Sciences. Again, let's first examine the faculty in this division who <b>are not</b> eligible for tenure. From 2007 to 2017, the percentage of female faculty members...
+
+after: <b>...increased substantially</b>, though the group has since neared parity. The <a target="_blank" href="http://senate.columbia.edu/archives/reports_archive/14-15/csw_pipeline%20report_2004-13.pdf">establishment of the Office of the Vice Provost for Diversity</a> played a significant role in this growth.
+
+before: Let's now consider the Natural Sciences faculty who <b>are tenured</b>. Did this group see the same growth in female representation as the faculty not on tenure track? In the graph below, draw how you think the female percentage of the tenured level of the Natural Sciences has changed in the past decade.
+
+after: Similar to the Humanities, female representation at the tenured level of the Natural Sciences struggled to keep up with that at the untenured level.
+
+[]
+`;
